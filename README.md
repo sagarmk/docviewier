dbeba59eac0c4f89c92ccb8d789940d850353577
sagarmk <blacksaint@saintsnest>
2023-04-15 07:08:41 -0600

    added qt5

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
+PDF Tool
+This is a simple desktop application for processing PDF files. It uses the PyQt5 library to create a graphical user interface (GUI), PyPDF2 for handling PDF file operations, and the pdf2image library for converting PDF pages to images. The application provides the following functionalities:
 
-# WebPageDisplay
+Split a PDF file into a new PDF containing only specified pages.
+Join multiple PDF files into a single PDF.
+Convert a PDF file into a series of images, one image per page.
+![Example image](/images/pdftool.png)
 
-This is a PyQt5 application that allows users to view webpages in a QWebEngineView. It also has a QLineEdit for entering URLs, a back button, and a forward button. The application stores the last 10 URLs in a history list and allows users to navigate back and forth between them.
+
+Code Overview
+The code begins with importing the necessary libraries and then defines the PDFTool class, which inherits from QMainWindow. The constructor of this class (__init__) sets up the user interface with buttons, labels, and spin boxes using a grid layout. Each button is connected to a specific functionality (splitting, joining, or converting PDFs).
+
+Splitting PDFs
+The split_pdf method is called when the 'Split PDF' button is clicked. It prompts the user to select an output PDF file and then extracts the specified pages (start and end pages) from the input PDF. A new PDF is created containing only the selected pages.
+
+Joining PDFs
+The join_pdf method is called when the 'Join PDFs' button is clicked. It prompts the user to select multiple PDF files and an output PDF file. The input PDFs are combined in the order they were selected, and the result is saved to the output file.
+
+Converting PDFs to Images
+The convert_to_image method is called when the 'Convert PDF to Images' button is clicked. It prompts the user to select an input PDF file and an output folder. Each page of the input PDF is then converted to an image (PNG format) and saved in the output folder with the file name format 'page_X.png', where X is the page number.
+
+Running the Application
+To run the application, execute the code in a Python environment with the required libraries (PyQt5, PyPDF2, and pdf2image) installed. When the PDFTool instance is created and shown, the GUI will appear, allowing the user to interact with the application and perform the various operations on PDF files.
--- main.py
+++ main.py
@@ -1,63 +1,108 @@
 import sys
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextBrowser, QLineEdit, QVBoxLayout, QHBoxLayout, QWidget, QPushButton
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QFileDialog, QPushButton, QGridLayout, QWidget,
+                             QLabel, QSpinBox, QSizePolicy)
+from PyQt5.QtCore import Qt
+import PyPDF2
+from PIL import Image
+from pdf2image import convert_from_path
 
-
-class WebPageDisplay(QMainWindow):
+class PDFTool(QMainWindow):
     def __init__(self):
         super().__init__()
-        
-        self.input_area = QLineEdit()
-        self.input_area.returnPressed.connect(self.load_url)
-        
-        self.browser = QWebEngineView()
-        
-        self.back_button = QPushButton("Back")
-        self.back_button.clicked.connect(self.go_back)
-        self.forward_button = QPushButton("Forward")
-        self.forward_button.clicked.connect(self.go_forward)
-        
-        layout = QVBoxLayout()
-        button_layout = QHBoxLayout()
-        button_layout.addWidget(self.back_button)
-        button_layout.addWidget(self.forward_button)
-        layout.addLayout(button_layout)
-        layout.addWidget(self.input_area)
-        layout.addWidget(self.browser)
-        
+
         central_widget = QWidget()
-        central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
-        self.resize(self.sizeHint())
-        
-        self.history = []
-        self.history_index = 0
-    
-    def load_url(self):
-        url = self.input_area.text()
-        if not url.startswith(("http://", "https://")):
-            url = "http://" + url
-        self.browser.load(QUrl(url))
-        self.history.append(url)
-        self.history_index += 1
-        if len(self.history) > 10:
-            self.history.pop(0)
-            self.history_index -= 1
-    
-    def go_back(self):
-        if self.history_index > 0:
-            self.history_index -= 1
-            self.browser.load(QUrl(self.history[self.history_index]))
-    
-    def go_forward(self):
-        if self.history_index < len(self.history) - 1:
-            self.history_index += 1
-            self.browser.load(QUrl(self.history[self.history_index]))
+        layout = QGridLayout(central_widget)
+
+        self.btn_select_pdf = QPushButton('Select PDF', self)
+        self.btn_select_pdf.clicked.connect(self.select_pdf)
+        layout.addWidget(self.btn_select_pdf, 0, 0)
+
+        self.btn_split_pdf = QPushButton('Split PDF', self)
+        self.btn_split_pdf.clicked.connect(self.split_pdf)
+        self.btn_split_pdf.setEnabled(False)
+        layout.addWidget(self.btn_split_pdf, 0, 1)
+
+        self.start_page_label = QLabel("Start Page:")
+        layout.addWidget(self.start_page_label, 1, 0)
+        self.spinBox_start_page = QSpinBox(self)
+        layout.addWidget(self.spinBox_start_page, 1, 1)
+
+        self.end_page_label = QLabel("End Page:")
+        layout.addWidget(self.end_page_label, 2, 0)
+        self.spinBox_end_page = QSpinBox(self)
+        layout.addWidget(self.spinBox_end_page, 2, 1)
+
+        self.btn_join_pdf = QPushButton('Join PDFs', self)
+        self.btn_join_pdf.clicked.connect(self.join_pdf)
+        layout.addWidget(self.btn_join_pdf, 3, 0, 1, 2)
+
+        self.btn_convert_to_image = QPushButton('Convert PDF to Images', self)
+        self.btn_convert_to_image.clicked.connect(self.convert_to_image)
+        layout.addWidget(self.btn_convert_to_image, 4, 0, 1, 2)
+
+        self.setWindowTitle('PDF Tool')
+        self.setGeometry(100, 100, 300, 200)
+
+    def select_pdf(self):
+        pdf_path, _ = QFileDialog.getOpenFileName(self, "Select PDF file", "", "PDF Files (*.pdf)")
+        if pdf_path:
+            self.pdf_path = pdf_path
+            self.btn_split_pdf.setEnabled(True)
+
+    def split_pdf(self):
+        if hasattr(self, 'pdf_path'):
+            output_path = QFileDialog.getSaveFileName(self, "Select Output Folder", "", "PDF Files (*.pdf)")
+            if output_path[0]:
+                start_page = int(self.spinBox_start_page.value())
+                end_page = int(self.spinBox_end_page.value())
+                
+                with open(self.pdf_path, 'rb') as file:
+                    reader = PyPDF2.PdfReader(file)
+                    writer = PyPDF2.PdfWriter()
+                    
+                    for page_num in range(start_page - 1, end_page):
+                        writer.add_page(reader.pages[page_num])
+                        
+                    with open(output_path[0], 'wb') as output_file:
+                        writer.write(output_file)
+
+    def join_pdf(self):
+        pdf_files, _ = QFileDialog.getOpenFileNames(self, "Select PDF files", "", "PDF Files (*.pdf)")
+        if len(pdf_files) < 2:
+            return
+
+        output_path = QFileDialog.getSaveFileName(self, "Select Output Folder", "", "PDF Files (*.pdf)")
+        if output_path[0]:
+            writer = PyPDF2.PdfWriter()
+
+            for pdf_file in pdf_files:
+                with open(pdf_file, 'rb') as file:
+                    reader = PyPDF2.PdfReader(file)
+
+                    for page_num in range(len(reader.pages)):
+                        writer.add_page(reader.pages[page_num])
+
+            with open(output_path[0], 'wb') as output_file:
+                writer.write(output_file)
+                output_file.close()
 
 
-if __name__ == '__main__':
+
+
+
+
+    def convert_to_image(self):
+        pdf_path, _ = QFileDialog.getOpenFileName(self, "Select PDF file", "", "PDF Files (*.pdf)")
+        if pdf_path:
+            output_path = QFileDialog.getExistingDirectory(self, "Select Output Folder")
+            if output_path:
+                images = convert_from_path(pdf_path)
+                for i, image in enumerate(images):
+                    image.save(f'{output_path}/page_{i + 1}.png', 'PNG')
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = WebPageDisplay()
+    window = PDFTool()
     window.show()
     sys.exit(app.exec_())
